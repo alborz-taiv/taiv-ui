6c83e70270e36c64558da27b0c0cb3f482000d33
shana-taiv <shana@taiv.tv>
2026-05-05 10:21:38 -0500

    Swap back to old index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,75 +1,78 @@
 //Data
-
-export { ChartCard } from './Data/Cards/ChartCard/ChartCard';
-export { PieChartCard } from './Data/Cards/PieChartCard/PieChartCard';
-export { StatsCard } from './Data/Cards/StatsCard/StatsCard';
-export { Chart } from './Data/Chart/Chart';
 export { CheckboxTable } from './Data/CheckboxTable/CheckboxTable';
+export { Chart } from './Data/Chart/Chart';
+export { ChartCard } from './Data/Cards/ChartCard/ChartCard';
 export { PieChart } from './Data/PieChart/PieChart';
-export { Progress } from './Data/Progress/Progress';
+export { PieChartCard } from './Data/Cards/PieChartCard/PieChartCard';
 export { RemovableItemList } from './Data/RemovableItemList/RemovableItemList';
+export { StatsCard } from './Data/Cards/StatsCard/StatsCard';
 export { StatsBadge } from './Data/StatsBadge/StatsBadge';
-export { Badge } from './Info/Badge/Badge';
-export { InfoCard } from './Info/InfoCard/InfoCard';
-export { FormModal } from './Info/Modals/FormModal/FormModal';
+export { Progress } from './Data/Progress/Progress';
+
 //Info
 export { Modal } from './Info/Modals/Modal/Modal';
-export { ModalProvider } from './Info/Modals/ModalProvider/ModalProvider';
+export { FormModal } from './Info/Modals/FormModal/FormModal';
 export { StepperModal } from './Info/Modals/StepperModal/StepperModal';
+export { ModalProvider } from './Info/Modals/ModalProvider/ModalProvider';
 export { NotificationProvider } from './Info/Notifications/NotificationProvider/NotificationProvider';
-export { FormulaTooltip } from './Info/Tooltips/FormulaTooltip/FormulaTooltip';
-export { InfoTooltip } from './Info/Tooltips/InfoTooltip/InfoTooltip';
+export { Badge } from './Info/Badge/Badge';
 export { Tooltip } from './Info/Tooltips/Tooltip/Tooltip';
+export { InfoTooltip } from './Info/Tooltips/InfoTooltip/InfoTooltip';
+export { FormulaTooltip } from './Info/Tooltips/FormulaTooltip/FormulaTooltip';
+export { InfoCard } from './Info/InfoCard/InfoCard';
+
+//Inputs
+export { AutoComplete } from './Inputs/TextInputs/AutoComplete/AutoComplete';
 export { Button } from './Inputs/Buttons/Button/Button';
 export { IconButton } from './Inputs/Buttons/IconButton/IconButton';
-export { SSOButton } from './Inputs/Buttons/SSOButton/SSOButton';
 export { UnstyledButton } from './Inputs/Buttons/UnstyledButton/UnstyledButton';
+export { SSOButton } from './Inputs/Buttons/SSOButton/SSOButton';
 export { Checkbox } from './Inputs/Controls/Checkbox/Checkbox';
 export { Radio } from './Inputs/Controls/Radio/Radio';
 export { RadioList } from './Inputs/Controls/RadioList/RadioList';
-export { SegmentedControl } from './Inputs/Controls/SegmentedControl/SegmentedControl';
 export { Toggle } from './Inputs/Controls/Toggle/Toggle';
-export { DatePicker } from './Inputs/Dates/DatePicker/DatePicker';
-export { CascadingSelect } from './Inputs/Dropdowns/CascadingSelect/CascadingSelect';
+export { SegmentedControl } from './Inputs/Controls/SegmentedControl/SegmentedControl';
 export { FontSelect } from './Inputs/Dropdowns/FontSelect/FontSelect';
 export { MultiSelect } from './Inputs/Dropdowns/MultiSelect/MultiSelect';
+export { CascadingSelect } from './Inputs/Dropdowns/CascadingSelect/CascadingSelect';
+export { SearchBar } from './Inputs/TextInputs/SearchBar/SearchBar';
 export { Select } from './Inputs/Dropdowns/Select/Select';
 export { Slider } from './Inputs/Sliders/Slider/Slider';
 export { VolumeSlider } from './Inputs/Sliders/VolumeSlider/VolumeSlider';
-//Inputs
-export { AutoComplete } from './Inputs/TextInputs/AutoComplete/AutoComplete';
-export { NumberInput } from './Inputs/TextInputs/NumberInput/NumberInput';
-export { PasswordInput } from './Inputs/TextInputs/PasswordInput/PasswordInput';
-export { SearchBar } from './Inputs/TextInputs/SearchBar/SearchBar';
 export { TextArea } from './Inputs/TextInputs/TextArea/TextArea';
 export { TextInput } from './Inputs/TextInputs/TextInput/TextInput';
+export { PasswordInput } from './Inputs/TextInputs/PasswordInput/PasswordInput';
+export { DatePicker } from './Inputs/Dates/DatePicker/DatePicker';
+export { NumberInput } from './Inputs/TextInputs/NumberInput/NumberInput';
 
 //Layout
 export { AutoGrid } from './Layout/AutoGrid/AutoGrid';
+export { Grid } from './Layout/Grid/Grid';
 export { Box } from './Layout/Box/Box';
-export { Card } from './Layout/Card/Card';
 export { Center } from './Layout/Center/Center';
 export { Divider } from './Layout/Divider/Divider';
 export { Frame } from './Layout/Frame/Frame';
-export { Grid } from './Layout/Grid/Grid';
 export { Group } from './Layout/Group/Group';
 export { Loader } from './Layout/Loader/Loader';
-export { SectionCard } from './Layout/SectionCard/SectionCard';
 export { Stack } from './Layout/Stack/Stack';
-export type { ColumnConfig } from './Layout/Table/Table';
+export { Card } from './Layout/Card/Card';
+export { SectionCard } from './Layout/SectionCard/SectionCard';
 export { Table } from './Layout/Table/Table';
+export type { ColumnConfig } from './Layout/Table/Table';
 export { Tabs } from './Layout/Tabs/Tabs';
-export { IconBadge } from './Misc/IconBadge/IconBadge';
-export { LoadingOverlay } from './Misc/LoadingOverlay/LoadingOverlay';
-export { Skeleton } from './Misc/Skeleton/Skeleton';
-export { Step } from './Misc/Stepper/Step';
-export { Stepper } from './Misc/Stepper/Stepper';
-//Misc
-export { Transition } from './Misc/Transition/Transition';
+
 //Typography
 export { CollapsibleText } from './Typography/CollapsibleText/CollapsibleText';
 export { CollapsibleTitle } from './Typography/CollapsibleTitle/CollapsibleTitle';
-export { Formula } from './Typography/Formula/Formula';
-export { Fraction } from './Typography/Fraction/Fraction';
 export { Text } from './Typography/Text/Text';
 export { Title } from './Typography/Title/Title';
+export { Fraction } from './Typography/Fraction/Fraction';
+export { Formula } from './Typography/Formula/Formula';
+
+//Misc
+export { Transition } from './Misc/Transition/Transition';
+export { IconBadge } from './Misc/IconBadge/IconBadge';
+export { LoadingOverlay } from './Misc/LoadingOverlay/LoadingOverlay';
+export { Skeleton } from './Misc/Skeleton/Skeleton';
+export { Stepper } from './Misc/Stepper/Stepper';
+export { Step } from './Misc/Stepper/Step';
